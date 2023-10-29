c24fc8277b0a9b09f75601667fc2bc2dbfa8e3fb
poovarasansivakumar2003 <132900038+poovarasansivakumar2003@users.noreply.github.com>
2023-10-29 19:00:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let name = prompt("Enter your name (up to 10 letters)");
+while (name === null || name === "" || name.length > 10) {
+    if (name === null || name === "") {
+        alert("Please enter your name.");
+    } else {
+        alert("Please enter your name with up to 10 letters.");
+    }
+    name = prompt("Enter your name (up to 10 letters)");
+}
+
+name = name.toUpperCase();
+alert('Welcome to UVCE ISE, ' + name);
+document.getElementById('userName').innerHTML = 'HELLO, ' + name;
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.onclick = function () {
+    hamburger.classList.toggle('close');
+    menu.classList.toggle('open');
+}
+
+const elts = {
+    text1: document.getElementById("text1"), text2: document.getElementById("text2")
+};
+const texts = ["Hello", name, "Welcome to UVCE ISE 26", "A", "Non-Profit", "Organisation", ":)", "Made by P=V^2/R"];
+const morphTime = 1;
+const cooldownTime = 0.25;
+let textIndex = texts.length - 1;
+let time = new Date(); let morph = 0;
+let cooldown = cooldownTime;
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+function doMorph() {
+    morph -= cooldown; cooldown = 0;
+    let fraction = morph / morphTime;
+    if (fraction > 1) {
+        cooldown = cooldownTime; fraction = 1;
+    }
+    setMorph(fraction);
+}
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    fraction = 1 - fraction; elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+function doCooldown() {
+    morph = 0; elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+function animate() {
+    requestAnimationFrame(animate);
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000; time = newTime;
+    cooldown -= dt;
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) { textIndex++; } doMorph();
+    }
+    else {
+        doCooldown();
+    }
+} animate();
